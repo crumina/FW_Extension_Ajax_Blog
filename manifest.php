90d121d12294893e83fb57fb3f21dcafe6928fef
Sergey Pererva <proger.su@gmail.com>
2019-02-14 16:46:31 +0200

    Update repo

--- manifest.php
+++ manifest.php
@@ -6,9 +6,9 @@ if ( !defined( 'FW' ) ) {
 
 $manifest = array();
 
-$manifest[ 'name' ]         = __( 'Ajax blog', 'fw' );
-$manifest[ 'description' ]  = __( 'Ajax blog.', 'fw' );
-$manifest[ 'remote' ]       = 'https://up.crumina.net/extensions/versions/';
+$manifest[ 'name' ]         = __( 'Ajax blog', 'crumina' );
+$manifest[ 'description' ]  = __( 'Ajax blog.', 'crumina' );
+$manifest[ 'github_repo' ]  = 'https://github.com/crumina/FW_Extension_Ajax_Blog';
 $manifest[ 'thumbnail' ]    = plugins_url( 'unyson/framework/extensions/ajax-blog/static/img/thumbnail.png' );
 $manifest[ 'version' ]      = '1.0.10';
 $manifest[ 'display' ]      = true;
